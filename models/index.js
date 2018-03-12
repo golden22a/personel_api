4902694d954645d38da950bf504c01174d7727fc
Abdelhalim <golden-boy1994@hotmail.fr>
2018-03-12 14:24:37 -0400

    added mongodb link

--- models/index.js
+++ models/index.js
@@ -1,4 +1,3 @@
 var mongoose = require('mongoose');
 /*add you connection somewhere here*/
-mongoose.connect('mongodb://localhost/personel_api', {promiseLibrary: global.Promise});
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/personel_api', {promiseLibrary: global.Promise});
